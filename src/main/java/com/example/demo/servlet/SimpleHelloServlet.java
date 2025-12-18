23ef50402313395c0e9178b62e0c486051c1d65a
bala-amypo <bala@amypo.in>
2025-12-18 08:48:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
@@ -0,0 +1,21 @@
+package com.example.demo.servlet;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class SimpleHelloServlet extends HttpServlet {
+        @Override
+
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+
+            throws IOException {
+
+            resp.setStatus(200);
+
+            resp.getWriter().write("OK");
+
+    }
+
+}
