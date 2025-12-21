4e0ab46a31a3c60d816c7b943dc27fbc804e2541
bala-amypo <bala@amypo.in>
2025-12-21 05:58:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/ResourceRequestRepository.java
+++ src/main/java/com/example/demo/repository/ResourceRequestRepository.java
@@ -2,16 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ResourceRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface ResourceRequestRepository extends JpaRepository<ResourceRequest, Long> {
-
     List<ResourceRequest> findByRequestedById(Long userId);
 
-    List<ResourceRequest> findByStartTimeBetween(
-            LocalDateTime start,
-            LocalDateTime end
-    );
+    List<ResourceRequest> findByStartTimeBetween(LocalDateTime start, LocalDateTime end);
 }
