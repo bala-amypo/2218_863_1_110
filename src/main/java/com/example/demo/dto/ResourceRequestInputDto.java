78a803eee8a413753987e20c3e6845daf4da323d
bala-amypo <bala@amypo.in>
2025-12-26 04:28:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ResourceRequestInputDto.java
@@ -0,0 +1,46 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+public class ResourceRequestInputDto {
+
+    private String resourceType;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String purpose;
+
+    public ResourceRequestInputDto() {
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+}
